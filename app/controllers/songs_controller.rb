5913665a59e2a0d59b958e1cb6b7b2ef4de96818
Michael Roose <mjroose@gmail.com>
2018-11-17 16:31:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,26 +1,30 @@
 class SongsController < ApplicationController
   def index
-
+    @songs = Artist.all
   end
 
   def show
-
+    @song = Artist.find(params[:id])
   end
 
   def new
-
+    @song = Artist.new
   end
 
   def create
-
+    @song = Artist.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
   end
 
   def edit
-
+    @song = Artist.find(params[:id])
   end
 
   def update
-
+    @song = Artist.find(params[:id])
+    @song.update(song_params)
+    redirect_to song_path(@song)
   end
 
   private
